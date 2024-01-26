b1e02e0da5528439c53eb9938acef81aef352fea
Pavel Druzhinin <p.a.druzhinin@tinkoff.ru>
2024-01-27 00:51:35 +0300

    Add routing. Now we have multiplexer or router!

--- main.go
+++ main.go
@@ -38,20 +38,27 @@ func handle(conn net.Conn) {
 			panic(err)
 		}
 	}()
-	h := request(conn)
-	response(conn, h)
+	u := request(conn)
+	switch u {
+	case "/":
+		response(conn)
+	case "/apply":
+		responseApply(conn)
+	default:
+		response(conn)
+	}
 }
 
 func request(conn net.Conn) string {
 	i := 0
 	s := bufio.NewScanner(conn)
-	var h string
+	var u string
 	for s.Scan() {
 		ln := s.Text()
 		fmt.Println(ln)
-		if i == 1 {
-			h = strings.Fields(ln)[1]
-			fmt.Println("***URL", h)
+		if i == 0 {
+			u = strings.Fields(ln)[1]
+			fmt.Println("***URI", u)
 		}
 		if ln == "" {
 			fmt.Println("Headers are done!")
@@ -60,24 +67,39 @@ func request(conn net.Conn) string {
 		i++
 	}
 
-	return h
+	return u
 }
 
-func response(conn net.Conn, s string) {
-	bh2 := `<!DOCTYPE html>
+func response(conn net.Conn) {
+	b := `<!DOCTYPE html>
 			<html lang="en">
 				<head>
 					<meta charset="UTF-8">
 					<title>Http Server</title>
 				</head>
 				<body>
-					<h2>Hello from `
-
-	ah2 := `!</h2>
+					<h2>It's just a starting page!'</h2>
 				</body>
 			</html>`
 
-	b := bh2 + s + ah2
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(b))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, b)
+}
+
+func responseApply(conn net.Conn) {
+	b := `<!DOCTYPE html>
+			<html lang="en">
+				<head>
+					<meta charset="UTF-8">
+					<title>Http Server</title>
+				</head>
+				<body>
+					<h2>You just applied this training!</h2>
+				</body>
+			</html>`
 
 	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
 	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(b))
